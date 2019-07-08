788ccd4aa2aabea22bfa9b1b98b66407fc1ba1a0
gks-spa <gks-spa@users.noreply.github.com>
2019-07-08 17:14:59 -0500

    Restructure difference_all_stats (output has now been sanity-checked a little)

--- difference_all_stats.py
+++ difference_all_stats.py
@@ -6,7 +6,6 @@
 #  - the lat/lon coordinates are rounded
 #  - For every 2080-2099 RCP4.5 variable with a delta dimension, delta=2 is now RCP4.5 minus RCP8.5
 #  - For every 2080-2099 RCP8.5 variable with a delta dimension, delta=2 is now RCP8.5 minus RCP4.5
-#  - Delta is now labeled: [absolute, vs_historical, vs_alternate]
 from xarray import DataArray
 import resources
 import xarray as xr
@@ -53,8 +52,7 @@ def get_all_stats_paths_relative():
 def get_stats_data_files():
     files = get_all_stats_paths_relative()
     for key in files:
-        files[key] = xr.open_dataset(resources.STATS_ROOT + files[key], chunks={})
-        resources.round_coords(files[key], {"latDim", "lonDim"})
+        files[key] = xr.open_dataset(resources.STATS_ROOT + files[key])
     return files
 
 
@@ -62,7 +60,7 @@ def get_stats_data_files():
 # sets delta=2 to be this minus other (similarly to how delta=1 is this minus historical)
 def concat_differences(data_this, data_other):
     data_new = {}
-    for variable in data_this:
+    for variable in data_this.variables:
         variable_this = data_this[variable]
         # We only want to work with variables that already have a historical delta (this excludes things like lat/lon)
         if "delta" in variable_this.dims:
@@ -71,36 +69,50 @@ def concat_differences(data_this, data_other):
             # We can't append, so we have to concatenate
             # noinspection PyTypeChecker
             variable_this = xr.concat([variable_this, difference], dim="delta")  # type: DataArray
-            variable_this = variable_this.assign_coords(delta=["absolute", "vs_historical", "vs_alternate"])
+            # See the assign_coords comment below
+            # variable_this = variable_this.assign_coords(delta=["absolute", "vs_historical", "vs_alternate"])
         data_new[variable] = variable_this
     return resources.format_dataset(data_new)
 
 
-# Does the same reformatting as concat_differences without actually adding any data (used for consistency)
-def reformat_similarly(data_this):
+# Removes an unused dimension
+# Could be greatly expanded in the future
+def reformat(data_this):
+    # Get rid of the singleton
+    data_this = data_this.squeeze()
     data_new = {}
-    for variable in data_this:
+    for variable in data_this.variables:
         variable_this = data_this[variable]
-        if "delta" in variable_this.dims:
-            variable_this = variable_this.assign_coords(delta=["absolute", "vs_historical"])
+        # This part doesn't seem to have the intended effect of assigning coordinates to delta (TODO: figure out why)
+        # if "delta" in variable_this.dims:
+        #     variable_this = variable_this.assign_coords(delta=["absolute", "vs_historical"])
         data_new[variable] = variable_this
     return resources.format_dataset(data_new)
 
 
 def main():
+    # Read files
     files = get_stats_data_files()
+
+    # Reformat
     for key in files:
-        output = None
+        files[key] = reformat(files[key])
+
+    # Calculate differences
+    for key in files:
+        output = files[key]
         if key[1] == "2080-2099":
             other_key = (key[0], key[1], "RCP4.5" if key[2] == "RCP8.5" else "RCP8.5" if key[2] == "RCP4.5" else None)
             output = concat_differences(files[key], files[other_key])
-        else:
-            output = reformat_similarly(files[key])
+        files[key] = output
+
+    # Write files
+    for key in files:
         path = resources.OUTPUT_ROOT + "new_ensemble_average_stats/" + get_stats_path_relative(*key)
         folder = os.path.dirname(path)
         if not os.path.exists(folder): os.makedirs(folder)  # Create the enclosing folder structure if necessary
         if os.path.exists(path): os.remove(path)  # Delete the existing file if necessary
-        output.to_netcdf(path=path)
+        files[key].to_netcdf(path=path)
 
 
 if __name__ == "__main__":
