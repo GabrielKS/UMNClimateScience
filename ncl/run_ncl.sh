b3baab283691475c9b9c4752fbbe4feb4dbee282
gks-spa <gks-spa@users.noreply.github.com>
2019-07-17 01:56:59 -0500

    Add t_test_example to compare to future Python code; add run_ncl NCL execution helper script

---
+++ ncl/run_ncl.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#  Written by Gabriel Konar-Steenberg in the summer of 2019.
+#  Part of a University of Minnesota Department of Soil, Water, and Climate climate modeling project.
+
+ncl $1  #options can be added here (e.g. -n to disable enumeration, -Q to disable NCL version, etc. info)
+#TODO: Maybe something can be done here to get more normal output in PyCharm
